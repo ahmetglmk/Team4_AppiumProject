4a54f54af8dfdcb25eed30310622e5a2885418da
mates <mates2041@gmail.com>
2024-06-29 09:50:27 +0300

    US_005/007/021 test caseleri bitti

--- src/test/java/Page/QueryCardPage.java
+++ src/test/java/Page/QueryCardPage.java
@@ -11,31 +11,63 @@ import org.openqa.selenium.support.PageFactory;
 import utilities.ConfigReader;
 import utilities.ReusableMethods;
 
+import javax.sound.midi.InvalidMidiDataException;
+
 import static org.junit.Assert.assertTrue;
 import static utilities.Driver.getAppiumDriver;
+import static utilities.OptionsMet.swipe;
 
 @Getter
 public class QueryCardPage {
    public QueryCardPage(){
        PageFactory.initElements(new AppiumFieldDecorator(getAppiumDriver()),this);
 
-    }
-    @AndroidFindBy(xpath = "(//android.widget.ImageView[1])[1]")
-    private WebElement queryCardLogoElement;
-    @AndroidFindBy (xpath = "(//android.widget.ImageView[1])[2]")
-    private WebElement searchBoxElement;
-    @AndroidFindBy (uiAutomator = "new UiSelector().description(\"Sign In\").instance(1)")
-    private WebElement signInLoginButton;
-    @AndroidFindBy(uiAutomator = "new UiSelector().className(\"android.widget.EditText\").instance(0)")
-    private WebElement phoneTextBox;
-    @AndroidFindBy(accessibility = "See All")
-    private WebElement seeAllIconElement;
-    @AndroidFindBy(uiAutomator = "new UiSelector().className(\"android.widget.EditText\").instance(1)")
-    private WebElement passwordTextBox;
-    @AndroidFindBy(uiAutomator = "new UiSelector().className(\"android.widget.ImageView\").instance(2)")
-    private WebElement rememberMeCheckBox;
-    @AndroidFindBy(uiAutomator = "new UiSelector().className(\"android.view.View\").instance(4)")
-    private WebElement addWishListToast;
+    }@AndroidFindBy(xpath = "(//android.widget.ImageView[1])[1]") private WebElement queryCardLogoElement;
+   @AndroidFindBy(uiAutomator = "new UiSelector().description(\"Profile\")")
+   private WebElement profileButton;
+   @AndroidFindBy (xpath = "(//android.widget.ImageView[1])[2]")
+   private WebElement searchBoxElement;
+   @AndroidFindBy (uiAutomator = "new UiSelector().description(\"Sign In\").instance(1)")
+   private WebElement signInLoginButton;
+   @AndroidFindBy(uiAutomator = "new UiSelector().className(\"android.widget.EditText\").instance(0)")
+   private WebElement phoneTextBox;
+   @AndroidFindBy(accessibility = "See All")
+   private WebElement seeAllIconElement;
+   @AndroidFindBy(uiAutomator = "new UiSelector().className(\"android.widget.EditText\").instance(1)")
+   private WebElement passwordTextBox;
+   @AndroidFindBy(uiAutomator = "new UiSelector().className(\"android.widget.ImageView\").instance(2)")
+   private WebElement rememberMeCheckBox;
+   @AndroidFindBy(uiAutomator = "new UiSelector().className(\"android.view.View\").instance(4)")
+   private WebElement addWishListToast;
+   @AndroidFindBy(xpath = "//android.view.View[@content-desc=\"Blossom Boutique\"]")
+   private   WebElement firstPopularBrands;
+   @AndroidFindBy(uiAutomator = "new UiSelector().className(\"android.widget.EditText\").instance(0)")
+   private WebElement signUpNameBox;
+   @AndroidFindBy(uiAutomator = "new UiSelector().description(\"Success\n" +
+           "Register Successfully.\")")
+   private WebElement successRegisterAlert;
+   @AndroidFindBy(uiAutomator = "new UiSelector().description(\"*Use Email Instead\")")
+   private WebElement useEmailButton;
+   @AndroidFindBy(uiAutomator = "new UiSelector().description(\"Enter Valid Email Address\")")
+   private WebElement invalidEmailText;
+   @AndroidFindBy(uiAutomator = "new UiSelector().description(\"Error\n" +
+           "The phone field must be at least 7 characters.\")")
+   private WebElement invalidPhoneNumberAlert;
+   @AndroidFindBy(uiAutomator = "new UiSelector().className(\"android.view.View\").instance(3)")
+   private WebElement invalidPasswordAlert;
+   @AndroidFindBy(uiAutomator = "new UiSelector().description(\"Sing In To See Your Info\")")
+   private WebElement signInToSeeText;
+   @AndroidFindBy(uiAutomator = "new UiSelector().description(\"Sign In\")")
+   private WebElement signInButton;
+   @AndroidFindBy(xpath = "(//*[@class='android.widget.EditText'])[2]")
+   private WebElement editPageEmailBox;
+   @AndroidFindBy(xpath = "(//*[@class='android.widget.EditText'])[3]")
+   private WebElement editPagePhoneBox;
+   @AndroidFindBy(accessibility = "Save Changes")
+   private WebElement editPageSaveChanges;
+   @AndroidFindBy(accessibility = "PROFILE_UPDATE\\nProfile Updated Successfully")
+   private WebElement profileUpdateAlert;
+
 
 
 
@@ -84,7 +116,120 @@ public class QueryCardPage {
         assertTrue(addWishListToast.isEnabled());
     }
 
+    public void seePopularBrands() throws InvalidMidiDataException {
+        for (int i = 0; i < 2; i++) {
+            swipe(600,2500,600,300);
+        }
+        assertTrue(firstPopularBrands.isDisplayed());
     }
+
+    public void signUpPhoneNumber(String fullName, String phoneNumber, String password) {
+        Actions actions = new Actions(getAppiumDriver());
+        assertTrue(signUpNameBox.isDisplayed());
+        signUpNameBox.click();
+        signUpNameBox.sendKeys(fullName);
+        ReusableMethods.wait(1);
+        actions.sendKeys(Keys.TAB).sendKeys(Keys.TAB)
+                .sendKeys(phoneNumber).perform();
+        ReusableMethods.wait(1);
+        actions.sendKeys(Keys.TAB).sendKeys(password).perform();
+        ReusableMethods.wait(1);
+        actions.sendKeys(Keys.TAB).sendKeys(Keys.ENTER).perform();
+        ReusableMethods.wait(1);
+        assertTrue(successRegisterAlert.isDisplayed());
+    }
+
+    public void signUpEmail(String fullName, String email, String password) {
+        Actions actions = new Actions(getAppiumDriver());
+        assertTrue(signUpNameBox.isDisplayed());
+        signUpNameBox.click();
+        signUpNameBox.sendKeys(fullName);
+        ReusableMethods.wait(1);
+        useEmailButton.click();
+        actions.sendKeys(Keys.TAB).sendKeys(Keys.TAB)
+                .sendKeys(email).perform();
+        ReusableMethods.wait(1);
+        actions.sendKeys(Keys.TAB).sendKeys(password).perform();
+        ReusableMethods.wait(1);
+        actions.sendKeys(Keys.TAB).sendKeys(Keys.ENTER).perform();
+        ReusableMethods.wait(1);
+        assertTrue(successRegisterAlert.isDisplayed());
+    }
+
+    public void signUpInvalidEmail(String fullName, String invalidEmail, String password) {
+        Actions actions = new Actions(getAppiumDriver());
+        assertTrue(signUpNameBox.isDisplayed());
+        signUpNameBox.click();
+        signUpNameBox.sendKeys(fullName);
+        ReusableMethods.wait(1);
+        useEmailButton.click();
+        actions.sendKeys(Keys.TAB).sendKeys(Keys.TAB)
+                .sendKeys(invalidEmail).perform();
+        ReusableMethods.wait(1);
+        actions.sendKeys(Keys.TAB).sendKeys(password).perform();
+        ReusableMethods.wait(1);
+        actions.sendKeys(Keys.TAB).sendKeys(Keys.ENTER).perform();
+        ReusableMethods.wait(1);
+        assertTrue(invalidEmailText.isDisplayed());
+    }
+
+    public void signUpInvalidPhoneNumber(String fullName, String invalidPhoneNumber, String password) {
+        Actions actions = new Actions(getAppiumDriver());
+        assertTrue(signUpNameBox.isDisplayed());
+        signUpNameBox.click();
+        signUpNameBox.sendKeys(fullName);
+        ReusableMethods.wait(1);
+        actions.sendKeys(Keys.TAB).sendKeys(Keys.TAB)
+                .sendKeys(invalidPhoneNumber).perform();
+        ReusableMethods.wait(1);
+        actions.sendKeys(Keys.TAB).sendKeys(password).perform();
+        ReusableMethods.wait(1);
+        actions.sendKeys(Keys.TAB).sendKeys(Keys.ENTER).perform();
+        ReusableMethods.wait(1);
+        assertTrue(invalidPhoneNumberAlert.isDisplayed());
+    }
+
+    public void signUpInvalidPassword(String fullName, String email, String invalidPassword) {
+        Actions actions = new Actions(getAppiumDriver());
+        assertTrue(signUpNameBox.isDisplayed());
+        signUpNameBox.click();
+        signUpNameBox.sendKeys(fullName);
+        ReusableMethods.wait(1);
+        useEmailButton.click();
+        actions.sendKeys(Keys.TAB).sendKeys(Keys.TAB)
+                .sendKeys(email).perform();
+        ReusableMethods.wait(1);
+        actions.sendKeys(Keys.TAB).sendKeys(invalidPassword).perform();
+        ReusableMethods.wait(1);
+        actions.sendKeys(Keys.TAB).sendKeys(Keys.ENTER).perform();
+        ReusableMethods.wait(1);
+        assertTrue(invalidPasswordAlert.isDisplayed());
+    }
+
+    public void login() {
+        Actions actions = new Actions(getAppiumDriver());
+        assertTrue(queryCardLogoElement.isDisplayed());
+        profileButton.click();
+        assertTrue(signInToSeeText.isDisplayed());
+        signInButton.click();
+        phoneTextBox.sendKeys(ConfigReader.getProperty("phoneNumberMurat"));
+        actions.sendKeys(Keys.TAB).perform();
+        actions.sendKeys(ConfigReader.getProperty("passwordMurat")).perform();
+        actions.sendKeys(Keys.TAB).sendKeys(Keys.TAB).sendKeys(Keys.ENTER).perform();
+    }
+
+    public void editProfile(String email, String phoneNumber) {
+        editPageEmailBox.click();
+        editPageEmailBox.clear();
+        editPageEmailBox.sendKeys(email);
+        editPagePhoneBox.click();
+        editPagePhoneBox.clear();
+        editPagePhoneBox.sendKeys(phoneNumber);
+        editPageSaveChanges.click();
+        ReusableMethods.wait(2);
+        assertTrue(profileUpdateAlert.isDisplayed());
+    }
+}
 
 
 
---
+++ src/test/resources/Features/US05_PopularBrands.feature
@@ -0,0 +1,8 @@
+Feature: US_005 Popular Brands
+  Background: User opens the app
+    * User makes driver adjustments
+  @MA
+  Scenario: See popular brands on the home page
+    * User confirms to be on the homepage
+    * Scrolls down to see popular brands
+    * Driver turns off
